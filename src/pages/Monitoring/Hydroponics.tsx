4f3352219b69711a011222996cca33b0a138e296
princejoogie <princejoogie@gmail.com>
2021-05-10 01:05:16 +0800

    qwe

--- src/pages/Monitoring/Hydroponics.tsx
+++ src/pages/Monitoring/Hydroponics.tsx
@@ -3,7 +3,12 @@ import React from "react";
 interface HydroponicsProps {}
 
 const Hydroponics: React.FC<HydroponicsProps> = () => {
-  return <div>Hydroponics</div>;
+  return (
+    <div>
+      Hydroponics
+      <p>Shaine Panget</p>
+    </div>
+  );
 };
 
 export default Hydroponics;
